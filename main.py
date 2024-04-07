450a437eeaccf2e45d11187163e0103b5fc45a9e
Paul Peytevin <ppeytevin@gmail.com>
2024-04-07 23:42:38 +0200

    real time capture

--- main.py
+++ main.py
@@ -8,3 +8,17 @@ import cv2
 
 segmentation_model = instance_segmentation()
 segmentation_model.load_model('mask_rcnn_coco.h5')
+
+### Real Time Capture
+
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+    
+    cv2.imshow('Instance Segmentation', frame)
+    
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
