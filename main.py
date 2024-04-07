62df42e358715d53c424c3d046482a3d99f0635c
Paul Peytevin <ppeytevin@gmail.com>
2024-04-07 23:36:48 +0200

    Setup model

--- main.py
+++ main.py
@@ -3,3 +3,8 @@
 import pixellib
 from pixellib.instance import  instance_segmentation
 import cv2
+
+### Setup model
+
+segmentation_model = instance_segmentation()
+segmentation_model.load_model('mask_rcnn_coco.h5')
